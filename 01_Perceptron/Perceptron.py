b4ebef5bd064d2f0e728fa275263ff075509c842
Lluís Camino <caminoperez.lluis@gmail.com>
2021-10-11 18:04:10 +0200

    Implement Perceptron

--- 01_Perceptron/Perceptron.py
+++ 01_Perceptron/Perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Perceptron:
     """Perceptron classifier.
 
@@ -18,6 +19,7 @@ class Perceptron:
         Number of misclassifications in every epoch.
 
     """
+
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
@@ -39,16 +41,23 @@ class Perceptron:
 
         """
         self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
 
-        # TODO: Put your code
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+
         return self
 
     def __net_input(self, X):
         """Calculate net input"""
-
-        # TODO: Put your code
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X):
-        """Return class label"""
-
-        # TODO: Put your code
+        """Return class label after unit step"""
+        return np.where(self.__net_input(X) >= 0.0, 1, -1)
--- 01_Perceptron/main.py
+++ 01_Perceptron/main.py
@@ -1,22 +1,36 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import make_classification
+
 from Perceptron import Perceptron
 
 # Generació del conjunt de mostres
 X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_repeated=0,
-                           n_classes=2, n_clusters_per_class=1, class_sep=1.0,
+                           n_classes=2, n_clusters_per_class=1, class_sep=0.8,
                            random_state=8)
-
 y[y == 0] = -1  # La nostra implementació esta pensada per tenir les classes 1 i -1.
 
-# TODO: descomentar en tenir implementat
-# perceptron = Perceptron()
-# perceptron.fit(X, y)
-# y_prediction = perceptron.predict(X)
+perceptron = Perceptron(eta=0.00000001, n_iter=100)
+perceptron.fit(X, y)
+y_prediction = perceptron.predict(X)
 
 #  Mostram els resultats
 plt.figure(1)
 
 plt.scatter(X[:, 0], X[:, 1], c=y)
+
+minim = np.min(X[:, 0])
+maxim = np.max(X[:, 0])
+
+first_coordinate = (minim, (-perceptron.w_[0] - (perceptron.w_[1] * minim)) / perceptron.w_[2])
+second_coordinate = (maxim, (-perceptron.w_[0] - (perceptron.w_[1] * maxim)) / perceptron.w_[2])
+
+plt.plot([first_coordinate[0], second_coordinate[0]], [first_coordinate[1], second_coordinate[1]])
+
+plt.figure(2)
+plt.plot(perceptron.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
 plt.show()
+
+print(perceptron.w_)
