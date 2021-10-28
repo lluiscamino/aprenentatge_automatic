7364e00b245ff02728316aa702b8b4f685c75c19
lluis <caminoperez.lluis@gmail.com>
2021-10-28 17:43:26 +0200

    Complete activity 06

--- 06_Parameter_adjustment/main.py
+++ 06_Parameter_adjustment/main.py
@@ -1,12 +1,9 @@
-import pandas as pd
-
 # Llibreries que necessitarem
-import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-
-import matplotlib.pyplot as plt
 
 random_value = 33
 # Carrega de dades i preparació de les dades emprant Pandas
@@ -17,3 +14,17 @@ valors = pd.DataFrame(data.iloc[:, -1])  # Seleccionam totes les files i la colu
 X = datos.to_numpy()
 y = valors.to_numpy().ravel()
 features_names = datos.columns
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_value)
+
+random_forest = RandomForestRegressor(random_state=random_value)
+parameters = {'max_depth': range(1, 602, 50), 'n_estimators': range(1, 602, 50)}
+grid_search_cv = GridSearchCV(random_forest, parameters, cv=3)
+grid_search_cv.fit(X_train, y_train)
+pd.set_option('display.max_columns', None)
+print(pd.DataFrame(grid_search_cv.cv_results_))
+print("Best estimator: ", grid_search_cv.best_estimator_)
+
+best_random_forest = grid_search_cv.best_estimator_
+best_random_forest.fit(X_train, y_train)
+y_pred = best_random_forest.predict(X_test)
+print("R2 score", r2_score(y_test, y_pred))
